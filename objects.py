df4850943c0e95bce2c24ed462a5818a2bec4e16
Carlo Brechbühler <carlo.brechbuhler@bluewin.ch>
2026-04-29 21:04:02 +0200

    ajouté prendre des objets

--- objects.py
+++ objects.py
@@ -3,14 +3,32 @@ class Piece:
         self.nom = nom
         self.description = description
         self.sorties = {}
+        self.objets = []
+        self.flags = {}
+
+    def ajouter_sortie(self, direction, piece_destination):
+        self.sorties[direction] = piece_destination
+
+    def ajouter_objet(self, objet):
+        self.objets.append(objet)
+
+    def trouver_objet(self, nom):
+        for objet in self.objets:
+            if  objet.nom == nom:
+                return objet
+        return None
 
     def decrire(self):
         print()
         print(self.nom)
         print(self.description)
 
-    def ajouter_sortie(self, direction, piece_destination):
-        self.sorties[direction] = piece_destination
+        if  self.objets:
+            print("Objets visibles :",", ".join(objet.nom for objet in self.objets))
+
+        if self.sorties:
+            print("Sorties :", ", ".join(self.sorties.keys()))
+
 
 class Objet:
     def __init__(self, nom, portable = True):
@@ -25,6 +43,12 @@ class Joueur:
     def __init__(self, position_depart):
         self.position = position_depart
         self.inventaire = []
+
+    def trouver_objet(self, nom):
+        for objet in self.inventaire:
+            if  objet.nom == nom:
+                return objet
+        return None
 
 
 class Engine:
@@ -42,6 +66,9 @@ class Engine:
             self.traiter_commande(commande)
 
     def traiter_commande(self, commande):
+        if  commande == "":
+            return
+        
         if  commande in ("quit", "exit"):
             self.en_cours = False
             print("Fin de la partie")
@@ -53,7 +80,8 @@ class Engine:
 
         if  verbe in ("prendre", "take"):
             self.prendre(complement)
-        else:        
+        else:
+            # pour nord, sud, est, ouest, entrer, sortir, monter, descendre ...        
             self.aller(commande)
 
     def aller(self, direction):
@@ -67,9 +95,23 @@ class Engine:
             print("Tu ne peux pas aller par là.")
 
     def prendre(self, nom_objet):
-        piece = self.joueur.position
-        objet = piece.tr
+        if  nom_objet == "":
+            print("Prendre quoi ?")
 
+        piece = self.joueur.position
+        objet = piece.trouver_objet(nom_objet)
+
+        if  objet is None:
+            print("Cet objet n'est pas ici")
+            return
+        
+        if  not objet.portable:
+            print("Tu ne peux pas rendre cet objet")
+            return
+        
+        piece.objets.remove(objet)
+        self.joueur.inventaire.append(objet)
+        print(f"Tu prends {objet.nom}")
 
 
 def creer_monde():
@@ -82,6 +124,11 @@ def creer_monde():
         "Tu es dans un vieux salon poussiéreux. Un tapis usé recouvre le sol."
     )
 
+    cave = Piece(
+        "Cave",
+        "Tu es dans une cave sombre et humide. L'air sent la pierre froide."
+    )
+
     foret = Piece(
         "Forêt",
         "Tu es dans une forêt calme. Les arbres entourent la maison"
@@ -92,14 +139,32 @@ def creer_monde():
     devant_maison.ajouter_sortie("nord", foret)
 
     salon.ajouter_sortie("sortir", devant_maison)
+    salon.ajouter_sortie("descendre", cave)
+
+    cave.ajouter_sortie("monter", salon)
 
     foret.ajouter_sortie("sud", devant_maison)
+
+    # Objets
+    lampe = Objet("lampe")
+    corde = Objet("corde")
+    boite = Objet("boîte aux lettres", portable=False)
+
+    # Localisation objets
+    devant_maison.ajouter_objet(boite)
+    salon.ajouter_objet(lampe)
+    cave.ajouter_objet(corde)
+
+    # États locaux
+    cave.flags["sombre"] = True
+    salon.flags["tapis déplacé"] = False
 
     # Point d'entrée        
     return devant_maison    
 
 
 # main
+# if __name__ == "__main__" : 
 
 piece_depart = creer_monde()
 jeu = Engine(piece_depart)
